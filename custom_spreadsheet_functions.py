acf7506170e72e27fe71a067921a7782771ca15d
Nate Rush <naterush1997@gmail.com>
2023-09-08 10:55:14 -0700

    add documentation to spreadsheet functions

--- custom_spreadsheet_functions.py
+++ custom_spreadsheet_functions.py
@@ -7,7 +7,31 @@ FAIL = 'Fail'
 
 
 def CHECK_NUMBER_DIFFERENCE(series_one, series_two, tolerance=0.0):
-    # If the difference between the columns is more than number_difference_tolerance, then display the difference, otherwise label it immaterial
+    """
+    {
+        "function": "CHECK_NUMBER_DIFFERENCE",
+        "description": "Returns 'Match' if the two columns are exactly equal, 'Immaterial' if the two columns are within the tolerance, and the difference otherwise.",
+        "search_terms": ["check", "number", "difference", "tolerance"],
+        "examples": [
+            "CHECK_NUMBER_DIFFERENCE(excel, db, 0.5)"
+        ],
+        "syntax": "CHECK_NUMBER_DIFFERENCE(number_column_one, number_column_two, tolerance)",
+        "syntax_elements": [
+            {
+                "element": "number_column_one",
+                "description": "The first number column to compare."
+            },
+            {
+                "element": "number_column_two",
+                "description": "The second number column to compare."
+            },
+            {
+                "element": "tolerance",
+                "description": "Differences less than the tolerance will be labeled immaterial."
+            }
+        ]
+    }
+    """
 
     # Calculate the difference between the two series
     diff_series = series_one - series_two
@@ -24,6 +48,31 @@ def CHECK_NUMBER_DIFFERENCE(series_one, series_two, tolerance=0.0):
 
 
 def CHECK_STRING_DIFFERENCE(series_one, series_two, similarity_threshold=100):
+    """
+    {
+        "function": "CHECK_STRING_DIFFERENCE",
+        "description": "Returns 'Match' if the two columns are exactly equal, 'Immaterial' if the two columns's similarity are greater than the tolerance, and the similarity of the strings otherwise.",
+        "search_terms": ["check", "string", "difference", "tolerance"],
+        "examples": [
+            "CHECK_STRING_DIFFERENCE(excel, db, 90)"
+        ],
+        "syntax": "CHECK_STRING_DIFFERENCE(string_column_one, string_column_two, similarity_threshold)",
+        "syntax_elements": [
+            {
+                "element": "string_column_one",
+                "description": "The first string column to compare."
+            },
+            {
+                "element": "string_column_two",
+                "description": "The second string column to compare."
+            },
+            {
+                "element": "similarity_threshold",
+                "description": "The minimum required similarity between the two strings. Two identical strings have a similarity of 100."
+            }
+        ]
+    }
+    """
 
     def label_fuzzy_ratio(value1, value2, similarity_threshold):
         ratio = fuzz.ratio(str(value1), str(value2))
